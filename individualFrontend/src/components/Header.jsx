9304495f21ec62d1259a0193a7b32f8a7f9ac0f6
p9co <ramazan01012005@gmail.com>
2024-04-02 22:53:39 +0300

    add homepage

--- individualFrontend/src/components/Header.jsx
+++ individualFrontend/src/components/Header.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import "../App.css";
 
 function Header() {
   const logo = "</>";
-  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+  const currentPath = window.location.pathname;
 
   return (
     <header className="header-task">
       <Link to="/" className={currentPath === '/' ? 'active' : ''}>{logo}</Link>
-      <Link to="/directory" className={currentPath === '/directory' ? 'active' : ''}>Справочник</Link>
       <Link to="/tasks" className={currentPath === '/tasks' ? 'active' : ''}>Задачи</Link>
       <Link to="/teacher" className={currentPath === '/teacher' ? 'active' : ''}>Учителю</Link>
     </header>
--- individualFrontend/src/pages/Home.jsx
+++ individualFrontend/src/pages/Home.jsx
@@ -1,11 +1,37 @@
-import "../App.css"
+import { Card } from "antd";
+import { FaGithub, FaTelegram } from "react-icons/fa";
+import { SlSocialVkontakte } from "react-icons/sl";
+import "../App.css";
+
 function Home() {
-  
   return (
     <>
-      <div className="container">Это главная страница </div>
+      <div className="container">
+        <div className="home">
+          <Card style={{ fontSize: 20 }}>
+            Я создал этот сайт с одной целью - помочь вам освоить математические
+            навыки и развить аналитическое мышление. Задачи добавляются учителем
+            математики/информатики{" "}
+            <a href="https://vk.com/oozimin">Олегом Олеговичем Зиминым</a>.
+          </Card>
+        </div>
+      </div>
+      <footer className="footer">
+        <a href="#">
+          <FaGithub style={{ fontSize: 30, color: "black" }} />
+        </a>
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab" onClick={() => window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab")}>
+          rickroll
+        </a>
+        <a href="https://vk.com/denprogrammers">
+          <SlSocialVkontakte style={{ fontSize: 30, color: "black" }} />
+        </a>
+        <a href="https://t.me/eclipsecode">
+          <FaTelegram style={{ fontSize: 30, color: "black" }} />
+        </a>
+      </footer>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
